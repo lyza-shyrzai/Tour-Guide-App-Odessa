e696ea280608eda85496962fde8b68bed1e8cb2a
Liza-S <lizash605@.gmail.com>
2017-09-18 20:27:17 +0300

    Added activities

---
+++ app/src/main/java/com/s/liza/tourguideappodessa/CafesActivity.java
@@ -0,0 +1,13 @@
+package com.s.liza.tourguideappodessa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CafesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cafes);
+    }
+}
---
+++ app/src/main/java/com/s/liza/tourguideappodessa/CatacombsActivity.java
@@ -0,0 +1,13 @@
+package com.s.liza.tourguideappodessa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CatacombsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catacombs);
+    }
+}
