2bc8d9c3e544c9cc932b1008ced7713f6ffc88c5
Liza-S <lizash605@.gmail.com>
2017-09-23 22:18:18 +0300

    Added entry to the category of attractions

--- app/src/main/java/com/s/liza/tourguideappodessa/MainActivity.java
+++ app/src/main/java/com/s/liza/tourguideappodessa/MainActivity.java
@@ -1,7 +1,9 @@
 package com.s.liza.tourguideappodessa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setCustomOnClickListener(R.id.hotel, HotelActivity.class);
+        setCustomOnClickListener(R.id.cafes, CafesActivity.class);
+        setCustomOnClickListener(R.id.fam_attract, FamAttrActivity.class);
+        setCustomOnClickListener(R.id.museums, MuseumsActivity.class);
+        setCustomOnClickListener(R.id.theaters, theatersActivity.class);
+        setCustomOnClickListener(R.id.catacombs, CatacombsActivity.class);
+    }
+
+    public void setCustomOnClickListener(int resourceID, final Class className) {
+        View view = findViewById(resourceID);
+        view.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(view.getContext(), className);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
