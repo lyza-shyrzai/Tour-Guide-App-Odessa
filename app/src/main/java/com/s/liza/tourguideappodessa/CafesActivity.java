921233b2ad7048a22b4d664507388ba655bbe6e5
Liza-S <lizash605@gmail.com>
2017-09-30 14:14:45 +0300

    Fixed Gradle dependencies for CardView and InfoAdapter and added Site Info

--- app/src/main/java/com/s/liza/tourguideappodessa/CafesActivity.java
+++ app/src/main/java/com/s/liza/tourguideappodessa/CafesActivity.java
@@ -2,6 +2,9 @@ package com.s.liza.tourguideappodessa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class CafesActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class CafesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loc_list);
+
+        ArrayList<Info> info = new ArrayList<Info>();
+        info.add(new Info("Bernardazzi", "Welcome to the restaurant \"Bernardazzi\" - a special world in which pleasant combinations of wine and food are decorated with live musical sketches.", "вулиця Буніна, 15", "График работы: Понедельник-Четверг с 12:00 до 00:00", "(48) 785-55-85", "bernardazzi.od.ua"));
+
+        InfoAdapter adapter = new InfoAdapter(this, info);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/s/liza/tourguideappodessa/Info.java
+++ app/src/main/java/com/s/liza/tourguideappodessa/Info.java
@@ -7,13 +7,15 @@ public class Info {
     private String mAddress;
     private String mWorkHours;
     private String mTel;
+    private String mSite;
 
-    public Info(String name, String desc, String address, String workHours, String tel) {
+    public Info(String name, String desc, String address, String workHours, String tel, String site) {
         mName = name;
         mDesc = desc;
         mAddress = address;
         mWorkHours = workHours;
         mTel = tel;
+        mSite = site;
     }
 
     public String getName() {
@@ -34,5 +36,9 @@ public class Info {
 
     public String getTel() {
         return mTel;
+    }
+
+    public String getSite() {
+        return mSite;
     }
 }
