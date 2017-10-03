969a15592297467e76db2c1e79079388d9c8d80b
Liza-S <lizash605@gmail.com>
2017-10-03 22:13:14 +0300

    Added Cafes Category

--- app/src/main/java/com/s/liza/tourguideappodessa/CafesActivity.java
+++ app/src/main/java/com/s/liza/tourguideappodessa/CafesActivity.java
@@ -14,7 +14,12 @@ public class CafesActivity extends AppCompatActivity {
         setContentView(R.layout.loc_list);
 
         ArrayList<Info> info = new ArrayList<Info>();
-        info.add(new Info(R.drawable.cafes_one, R.string.one_hotel_name, "Welcome to the restaurant \"Bernardazzi\" - a special world in which pleasant combinations of wine and food are decorated with live musical sketches.", "вулиця Буніна, 15", "График работы: Понедельник-Четверг с 12:00 до 00:00", "(48) 785-55-85", "bernardazzi.od.ua"));
+        info.add(new Info(R.drawable.cafes_one, R.string.one_cafe_name, R.string.one_cafe_desc, R.string.one_cafe_addr, R.string.one_cafe_work, R.string.one_cafe_tel, R.string.one_cafe_site));
+        info.add(new Info(R.drawable.cafes_two, R.string.two_cafe_name, R.string.two_cafe_desc, R.string.two_cafe_addr, R.string.two_cafe_work, R.string.two_cafe_tel, R.string.two_cafe_site));
+        info.add(new Info(R.drawable.cafes_three, R.string.three_cafe_name, R.string.three_cafe_desc, R.string.three_cafe_addr, R.string.three_cafe_work, R.string.three_cafe_tel, R.string.three_cafe_site));
+        info.add(new Info(R.drawable.cafes_four, R.string.four_cafe_name, R.string.four_cafe_desc, R.string.four_cafe_addr, R.string.four_cafe_work, R.string.four_cafe_tel, R.string.four_cafe_site));
+        info.add(new Info(R.drawable.cafes_five, R.string.five_cafe_name, R.string.five_cafe_desc, R.string.five_cafe_addr, R.string.five_cafe_work, R.string.five_cafe_tel, R.string.five_cafe_site));
+
 
         InfoAdapter adapter = new InfoAdapter(this, info);
         ListView listView = (ListView) findViewById(R.id.list);
